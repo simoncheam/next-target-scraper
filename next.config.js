ce4388dbb0b39bdd2929be95ff550fdd461e8df3
Simon <81940817+simoncheam@users.noreply.github.com>
2023-07-23 20:54:17 -0400

    deployment debug

--- next.config.js
+++ next.config.js
@@ -15,6 +15,14 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      bufferutil: false,
+      'utf-8-validate': false
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
